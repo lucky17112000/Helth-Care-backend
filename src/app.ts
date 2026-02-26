c2840d0c5db4110a29f7cdd2c591038299f995dd
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-02-26 18:48:50 +0600

    prisma integration complete

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express, { Application, Request, Response } from "express";
+import { prisma } from "./app/lib/prisma";
 const app: Application = express();
 app.use(express.json()); // Add this line to enable JSON parsing in the request body
 app.use(express.urlencoded({ extended: true })); // Add this line to enable URL-encoded data parsing in the request body
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+app.get("/", async (req: Request, res: Response) => {
+  const speciality = await prisma.speciality.create({
+    data: {
+      title: "Cardiology",
+    },
+  });
+  res.status(200).json({
+    success: true,
+    data: speciality,
+    message: "Speciality created successfully",
+  });
 });
 export default app;
