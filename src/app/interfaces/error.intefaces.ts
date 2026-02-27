b08d2dfaef1757eae8aa4844b6040c6912b066b4
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-02-27 23:21:57 +0600

    global error handler polish and zod integrate

---
+++ src/app/interfaces/error.intefaces.ts
@@ -0,0 +1,10 @@
+export interface TErrorSource {
+  path: string;
+  message: string;
+}
+export interface TErrorResponse {
+  success: boolean;
+  message: string;
+  errorSource: TErrorSource[];
+  error?: any;
+}
--- src/app/middlware/globalErrorHandler.ts
+++ src/app/middlware/globalErrorHandler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../../config/env";
 import status from "http-status";
+import z from "zod";
+import { TErrorResponse, TErrorSource } from "../interfaces/error.intefaces";
 
 export const globalErrorHandler = (
   err: any,
@@ -11,11 +13,23 @@ export const globalErrorHandler = (
   if (envVars.NODE_ENV === "development") {
     console.error("Global Error:", err);
   }
+  const errorSource: TErrorSource[] = [];
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
   let message: string = "Internal Server Error";
+  if (err instanceof z.ZodError) {
+    statusCode = status.BAD_REQUEST;
+    message = "Zod Validation Error";
+    err.issues.forEach((issue) => {
+      errorSource.push({
+        path: issue.path.join(" => "),
+        message: issue.message,
+      });
+    });
+  }
   res.status(statusCode).json({
     success: false,
     message: message,
-    error: err.message,
-  });
+    errorSource,
+    error: envVars.NODE_ENV === "development" ? err : undefined,
+  } as TErrorResponse);
 };
